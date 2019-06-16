fee92f23b9d0757dd4f6979e23f6b2fb54274c63
Terry M. Poulin <BigBoss1964@gmail.com>
2019-06-16 14:44:25 -0400

    Add venv activation to m/m.cmd.
    Might be worth while to change using ./python as the venv to something
    more like ./python-linux-x86_64, so that
    you can do side by side development on
    multiple platforms.

--- m
+++ m
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "Loading venv"
+    . ./python/bin/activate
+fi
+
 python setup.py sdist bdist_wheel
 
 pip install --upgrade ./dist/RSS2Diaspora_Spider*-py3-none-any.whl
--- m.cmd
+++ m.cmd
@@ -1,5 +1,14 @@
 @ECHO OFF
 
+IF EXIST %VIRTUAL_ENV% GOTO do_m
+
+:load_venv
+    ECHO Loading venv
+    CALL .\python\Scripts\activate.bat
+    GOTO do_m
+
+:do_m
+
 python setup.py sdist bdist_wheel
 
 pip install --upgrade .\dist\RSS2Diaspora_Spider*-py3-none-any.whl
